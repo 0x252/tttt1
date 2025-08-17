be1eb992ce8e88b7b699129f05f8429ccc094f01
Alexander Y <startyourday60@gmail.com>
2025-08-17 12:50:21 +0000

    sea_level

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -1,23 +1,24 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.stats import linregress
+import numpy as np
 
 def draw_plot():
-    # Read data from file
+    df = pd.read_csv('epa-sea-level.csv')
 
+    fig, ax = plt.subplots(figsize=(10,6))
 
-    # Create scatter plot
+    plt.scatter(df['Year'], df['NOAA Adjusted Sea Level'], color='blue')
 
+    slope, intercept, r_value, p_value, std_err = linregress(df['Year'], df['NOAA Adjusted Sea Level'])
+    years_extended = np.arange(df['Year'].min(), 2051, 1)
+    line = slope * years_extended + intercept
+    ax.plot(years_extended, line, color='red')
 
-    # Create first line of best fit
+    ax.set_xlabel("Year")
+    ax.set_ylabel("Sea Level (inches)")
+    ax.set_title("Rise in Sea Level")
+    ax.set_xticks([1850, 1875, 1900, 1925, 1950, 1975, 2000, 2025, 2050, 2075])
 
-
-    # Create second line of best fit
-
-
-    # Add labels and title
-
-    
-    # Save plot and return data for testing (DO NOT MODIFY)
     plt.savefig('sea_level_plot.png')
     return plt.gca()
