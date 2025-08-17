d9ebb7d2335a427447255d620fd7ca3ef5f7fef2
Alexander Y <startyourday60@gmail.com>
2025-08-17 13:18:55 +0000

    feat: refactoring

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -5,20 +5,18 @@ import numpy as np
 
 def draw_plot():
     df = pd.read_csv('epa-sea-level.csv')
+    fig, ax = plt.subplots(figsize=(12,6))
+    sizes = df['CSIRO Adjusted Sea Level'] * np.pi*3
+    colors = df['CSIRO Adjusted Sea Level']
+    sc = ax.scatter(df['Year'], df['CSIRO Adjusted Sea Level'], c=colors, s=sizes)
+    for start, color in [(df['Year'].min(), 'red'), (2000, 'green')]:
+        d = df[df['Year'] >= start]
+        s, i, *_ = linregress(d['Year'], d['CSIRO Adjusted Sea Level'])
+        ax.plot(np.arange(start, 2051), s*np.arange(start, 2051)+i, color=color)
 
-    fig, ax = plt.subplots(figsize=(10,6))
-
-    plt.scatter(df['Year'], df['NOAA Adjusted Sea Level'], color='blue')
-
-    slope, intercept, r_value, p_value, std_err = linregress(df['Year'], df['NOAA Adjusted Sea Level'])
-    years_extended = np.arange(df['Year'].min(), 2051, 1)
-    line = slope * years_extended + intercept
-    ax.plot(years_extended, line, color='red')
-
-    ax.set_xlabel("Year")
-    ax.set_ylabel("Sea Level (inches)")
-    ax.set_title("Rise in Sea Level")
-    ax.set_xticks([1850, 1875, 1900, 1925, 1950, 1975, 2000, 2025, 2050, 2075])
-
+    ax.set(title="Rise in Sea Level", xlabel="Year", ylabel="Sea Level (inches)")
+    ax.set_xticks([1850,1875,1900,1925,1950,1975,2000,2025,2050,2075])
+    cbar = plt.colorbar(sc, ax=ax)
+    cbar.set_label('Sea Level (inches)')
     plt.savefig('sea_level_plot.png')
-    return plt.gca()
+    return ax
